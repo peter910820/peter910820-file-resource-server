8be0314ce0f154156cbd0d75e0397e4e4d1e0372
peter910820 <peter910820@gmail.com>
2025-01-13 21:15:36 +0800

    complete basic version

--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module peter910820-file-resource-server
 go 1.23.1
 
 require (
+	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+	github.com/CloudyKit/jet/v6 v6.2.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/gofiber/fiber/v2 v2.52.6 // indirect
+	github.com/gofiber/template v1.8.3 // indirect
+	github.com/gofiber/template/ace/v2 v2.1.8 // indirect
+	github.com/gofiber/template/jet/v2 v2.1.11 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -15,5 +21,6 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/yosssi/ace v0.0.5 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,71 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/jet/v2"
+
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	os.MkdirAll("./image", os.ModePerm)
 	// logrus settings
 	logrus.SetFormatter(&logrus.TextFormatter{
 		ForceColors:   true,
 		FullTimestamp: true,
 	})
 	logrus.SetLevel(logrus.DebugLevel)
+	// Create a new engine
+	engine := jet.New("./views", ".jet")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Static("/public", "./public")
+	app.Static("/image", "./image")
 
-	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("This is index page!")
+		return c.Render("index", fiber.Map{
+			"Title": "資源伺服器目錄",
+		}, "layouts/base")
+	})
+
+	app.Get("/folder", func(c *fiber.Ctx) error {
+		dir := "./image"
+		fileName := []string{}
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				logrus.Error(err)
+				return err
+			}
+			if !info.IsDir() {
+				fileName = append(fileName, path)
+			}
+			return nil
+		})
+		if err != nil {
+			logrus.Error(err)
+			return c.Status(fiber.StatusInternalServerError).SendString("server has an error")
+		}
+		return c.Render("folder", fiber.Map{
+			"FileName": fileName,
+		}, "layouts/base")
+	})
+
+	app.Post("/api/upload", func(c *fiber.Ctx) error {
+		file, err := c.FormFile("file")
+		if err != nil {
+			logrus.Error(err)
+			return c.Status(fiber.StatusBadRequest).SendString("uploaded failed")
+		}
+		err = c.SaveFile(file, fmt.Sprintf("./image/%s", file.Filename))
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("save file failed")
+		}
+		return c.Status(fiber.StatusOK).SendString("save file successful!")
 	})
 
 	logrus.Fatal(app.Listen(":3000"))
